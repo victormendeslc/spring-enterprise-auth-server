ce5294e19e67167f286fa0b6109d87ab1021eed8
Carlos Perche <carlos.perche@groupsoftware.com.br>
2020-01-06 13:42:30 -0300

    config

--- src/main/java/com/spring/enterprise/authserver/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/spring/enterprise/authserver/config/AuthorizationServerConfiguration.java
@@ -1,10 +1,12 @@
 package com.spring.enterprise.authserver.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Slf4j
+@Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
 
--- src/main/java/com/spring/enterprise/authserver/config/WebSecurityConfiguration.java
+++ src/main/java/com/spring/enterprise/authserver/config/WebSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Slf4j
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
