2a62ca312f8147e72b12461cf82ceef3ad93974d
Carlos Perche <carlos.perche@groupsoftware.com.br>
2020-01-08 13:18:07 -0300

    foo

--- src/main/java/com/spring/enterprise/authserver/domain/Entity.java
+++
@@ -1,6 +0,0 @@
-package com.spring.enterprise.authserver.domain;
-
-import java.io.Serializable;
-
-public interface Entity extends Serializable {
-}
--- src/main/java/com/spring/enterprise/authserver/dto/DTO.java
+++
@@ -1,7 +0,0 @@
-package com.spring.enterprise.authserver.dto;
-
-import java.io.Serializable;
-
-public interface DTO extends Serializable {
-
-}
