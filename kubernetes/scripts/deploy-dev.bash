240d70ee7ff6602fb71aa87d6bcac6aa64b5c1d2
Carlos Perche <carlos.perche@groupsoftware.com.br>
2020-01-08 16:37:21 -0300

    k8s

--- kubernetes/scripts/deploy-dev.bash
+++ kubernetes/scripts/deploy-dev.bash
@@ -7,18 +7,18 @@ set -ex
 # adding a line to the file /etc/hosts with the following command:
 # sudo bash -c "echo $(minikube ip) minikube.me | tee -a /etc/hosts"
 
-kubectl create configmap auth-server-configmap --from-file=/src/main/resources/application.yml --save-config
+kubectl create configmap auth-server-configmap --from-file=src/main/resources/application.yml -o yaml --dry-run | kubectl apply -f -
 
 kubectl create secret generic postgres-server-credentials \
     --from-literal=POSTGRES_DB=auth_db \
     --from-literal=POSTGRES_USER=postgres \
     --from-literal=POSTGRES_PASSWORD=postgres \
-    --save-config
+    -o yaml --dry-run | kubectl apply -f -
 
 kubectl create secret generic postgres-credentials \
     --from-literal=SPRING_DATASOURCE_USERNAME=postgres \
     --from-literal=SPRING_DATASOURCE_PASSWORD=postgres \
-    --save-config
+    -o yaml --dry-run | kubectl apply -f -
 
 #kubectl create secret tls tls-certificate --key kubernetes/cert/tls.key --cert kubernetes/cert/tls.crt
 
@@ -29,5 +29,3 @@ kubectl wait --timeout=600s --for=condition=ready pod --all
 # Next deploy the microservices and wait for their pods to become ready
 kubectl apply -k kubernetes/services/overlays/dev
 kubectl wait --timeout=600s --for=condition=ready pod --all
-
-set +ex
